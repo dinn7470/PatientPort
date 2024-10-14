f829ac78b3d923001b0d68eb07f47205fc1da90d
Abhijit Nanda <abhijit.nanda@hyscaler.com>
2024-10-14 14:57:25 +0530

    navbar & header section added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import Doctors from "./pages/Doctors.jsx";
+import MyAppointments from "./pages/MyAppointments.jsx";
+import MyProfile from "./pages/MyProfile.jsx";
+import Login from "./pages/Login.jsx";
+import Contact from "./pages/Contact.jsx";
+import Appointments from "./pages/Appointments.jsx";
+import Navbar from "./components/Navbar.jsx";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="mx-4 sm:mx-[10%]">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/doctors" element={<Doctors />} />
+        <Route path="/doctors/:speciality" element={<Doctors />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/my-profile" element={<MyProfile />} />
+        <Route path="/my-appointments" element={<MyAppointments />} />
+        <Route path="/appointment/:docId" element={<Appointments />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
