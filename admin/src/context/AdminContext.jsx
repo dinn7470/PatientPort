6c3aed25b54ff41a3a8937c59cf88b1070781cd0
Abhijit Nanda <abhijit.nanda@hyscaler.com>
2024-10-27 21:14:24 +0530

    UI of showing all appointments

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -2,12 +2,15 @@ import { createContext, useState } from "react";
 import axios from "axios";
 export const AdminContext = createContext();
 import { toast } from "react-toastify";
+import { useEffect } from "react";
 
 const AdminContextProvider = (props) => {
   const [aToken, setAToken] = useState(
     localStorage.getItem("aToken") ? localStorage.getItem("aToken") : ""
   );
   const [doctors, setDoctors] = useState([]);
+  const [appointments, setAppointments] = useState([]);
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
   const getAllDoctors = async () => {
     try {
       const { data } = await axios.post(
@@ -46,7 +49,43 @@ const AdminContextProvider = (props) => {
       toast.error(error.message);
     }
   };
-  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+  const getAllAppointments = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + "/api/admin/appointments", {
+        headers: { aToken },
+      });
+      if (data.success) {
+        setAppointments(data.appointments);
+        console.log(data.appointments);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error(error.message);
+    }
+  };
+
+  const cancelAppointment = async (appointmentId) => {
+    try {
+      const { data } = await axios.post(
+        backendUrl + "/api/admin/cancel-appointments",
+        { appointmentId },
+        { headers: { aToken } }
+      );
+      if (data.success) {
+        toast.success(data.message);
+        getAllAppointments();
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error(error.message);
+    }
+  };
+
   const value = {
     aToken,
     setAToken,
@@ -55,6 +94,10 @@ const AdminContextProvider = (props) => {
     setDoctors,
     getAllDoctors,
     changeAvailability,
+    appointments,
+    setAppointments,
+    getAllAppointments,
+    cancelAppointment,
   };
   return (
     <AdminContext.Provider value={value}>
