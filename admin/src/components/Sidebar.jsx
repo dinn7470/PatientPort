1dfbb22b9faade6ce95c698d522dc5f25e8962a6
Abhijit Nanda <abhijit.nanda@hyscaler.com>
2024-11-01 09:18:41 +0530

    updated

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -60,12 +60,13 @@ const Sidebar = () => {
       {dToken && (
         <ul className="text-[#515151] mt-5">
           <NavLink
+            end
             className={({ isActive }) =>
               `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-64 cursor-pointer ${
                 isActive ? "bg-[#F2F3FF] border-r-4 border-primary " : ""
               }`
             }
-            to={"/doctor"}
+            to={"/doctor/"}
           >
             <img src={assets.home_icon} alt="" />
             <p className="hidden md:block">Dashboard</p>
--- admin/src/context/AppContext.jsx
+++ admin/src/context/AppContext.jsx
@@ -26,7 +26,7 @@ const AppContextProvider = (props) => {
   const calculateAge = (dob) => {
     const today = new Date();
     const birthDate = new Date(dob);
-    const age = today.getFullYear() - birthDate.getFullYear();
+    let age = today.getFullYear() - birthDate.getFullYear();
     const month = today.getMonth() - birthDate.getMonth();
     if (month < 0 || (month === 0 && today.getDate() < birthDate.getDate())) {
       age--;
